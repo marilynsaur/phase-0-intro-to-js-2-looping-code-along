2d69897cf3572c5808231fabb194116fe596cc97
Marilyn Wren <Marilynwren64@yahoo.com>
2022-01-13 13:16:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+const thankYouCards = []; 
+function writeCards(thankYouCards, event) {
+  for (let i = 0; i < thankYouCards.length; i++) {
+    thankYouCards.splice(i,1, `Thank you, ${thankYouCards[i]}, for the wonderful ${event} gift!`)
+  }
+    return thankYouCards;
+}
+    
+  
+function countDown (){
+let countDown = 0;
+while (countDown <11) {
+    console.log(countDown++);
+}
+
+}
